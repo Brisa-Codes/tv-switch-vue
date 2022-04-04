f3912a9b8a6b69984ca73196e1cb22bf5f548912
Brisa-Codes <brisamukunde1@gmail.com>
2022-04-04 19:30:50 +0300

    TV switching

---
+++ script.js
@@ -0,0 +1,13 @@
+Vue.createApp({
+    
+    data(){
+        return {
+            appTitle: 'Vue Gang Tv',
+            btn: 'switch',
+            nextBtn: 'Next',
+            prevBtn: 'Previous',
+            visibility: true
+        }
+    }
+
+}).mount("#app")
